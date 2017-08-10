580060fdb3c3b861f94d2eae329d0bab03669d85
woonandez <matthew.allen.fernandez@gmail.com>
2017-08-10 12:31:09 -0700

    thumbnails should open full picture in a new tab

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,12 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   photos = [
-    'https://preview.ibb.co/mDJHxF/Screen_Shot_2017_08_10_at_10_07_53_AM.png',
-    'https://preview.ibb.co/nQg9Ba/Screen_Shot_2017_08_10_at_10_08_06_AM.png',
-    'https://preview.ibb.co/cwDpBa/Screen_Shot_2017_08_10_at_10_08_18_AM.png',
-    'https://preview.ibb.co/eAnhWa/Screen_Shot_2017_08_10_at_10_08_40_AM.png',
-    'https://preview.ibb.co/ghzwra/its_Cut_Time_thumb.png',
-    'https://preview.ibb.co/mW4qHF/its_Cut_Time1.png'];
+    {
+      thumbnail:'https://preview.ibb.co/mDJHxF/Screen_Shot_2017_08_10_at_10_07_53_AM.png',
+      full: 'https://ibb.co/dbLmra'
+    },
+    {
+      thumbnail: 'https://preview.ibb.co/nQg9Ba/Screen_Shot_2017_08_10_at_10_08_06_AM.png',
+      full: 'https://ibb.co/mjOd4v'
+    },
+    {
+      thumbnail: 'https://preview.ibb.co/cwDpBa/Screen_Shot_2017_08_10_at_10_08_18_AM.png',
+      full: 'https://ibb.co/hLHGra'
+    },
+    {
+      thumbnail: 'https://preview.ibb.co/eAnhWa/Screen_Shot_2017_08_10_at_10_08_40_AM.png',
+      full: 'https://ibb.co/dcB9Ba'
+    },
+    {
+      thumbnail: 'https://preview.ibb.co/ghzwra/its_Cut_Time_thumb.png',
+      full: 'https://ibb.co/idiNWa'
+    },
+    {
+      thumbnail: 'https://preview.ibb.co/mW4qHF/its_Cut_Time1.png',
+      full: 'https://ibb.co/ckeeBa'
+    }];
 
   constructor() {}
 
@@ -22,5 +40,8 @@ export class ProjectsComponent implements OnInit {
 }
 
 
-// https://ibb.co/idiNWa
-// https://ibb.co/ckeeBa
+
+
+
+
+
