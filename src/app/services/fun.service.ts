d202daf52e614ae7a6bc78b419bcf03e3233620c
woonandez <matthew.allen.fernandez@gmail.com>
2017-08-16 11:43:41 -0700

    add fun service to hold photos

---
+++ src/app/services/fun.service.ts
@@ -0,0 +1,16 @@
+export class FunService {
+  photos = [
+    {
+      thumbnail: "https://preview.ibb.co/iCiVNF/soccer.jpg",
+      full: "https://ibb.co/gGz4hF"
+    },
+    {
+      thumbnail: "https://preview.ibb.co/iRSJ9v/IMG_1954.jpg",
+      full: "https://ibb.co/dA6pGa"
+    },
+    {
+      thumbnail: "https://preview.ibb.co/ioRLpv/IMG_3569.jpg",
+      full: "https://ibb.co/dZoY9v"
+    }
+  ]
+}
