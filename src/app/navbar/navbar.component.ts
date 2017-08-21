54f301d6d51d685147a6e89f1aaba080a3c2427b
woonandez <matthew.allen.fernandez@gmail.com>
2017-08-21 08:34:31 -0700

    remove angular animations to favor native css animations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
-})
+
 export class NavbarComponent implements OnInit {
 
   constructor() { }
