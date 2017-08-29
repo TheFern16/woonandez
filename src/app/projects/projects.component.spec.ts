564b0c4a54fbfb0702ef6746a1194895c87c4f0e
woonandez <matthew.allen.fernandez@gmail.com>
2017-08-29 11:27:06 -0700

    inject photo component into karma specs

--- src/app/projects/projects.component.spec.ts
+++ src/app/projects/projects.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectsComponent } from './projects.component';
+import { PhotoComponent } from './photo/photo.component';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
@@ -8,7 +9,7 @@ describe('ProjectsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectsComponent ]
+      declarations: [ ProjectsComponent, PhotoComponent ]
     })
     .compileComponents();
   }));
