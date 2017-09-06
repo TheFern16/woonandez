8a8fc74d6e56f642cbdb815cc0fedf20544de42a
woonandez <matthew.allen.fernandez@gmail.com>
2017-09-06 11:01:05 -0700

    create oxford comma rxjs observable

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,3 +1,13 @@
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
 export class RequestService {
-  constructor() {}
+  constructor(private http: Http) {}
+
+  addOxfordComma(text: string) {
+    const headers = new Headers({ 'Content-Type': 'applicaton/json' });
+    return this.http.post('/api/oxfordComma', text, { headers });
+  }
+
+
 }
