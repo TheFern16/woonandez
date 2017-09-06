86f2412cfe460b93f01537e03f422f67e1ea648b
woonandez <matthew.allen.fernandez@gmail.com>
2017-09-06 10:05:08 -0700

    add body parser middleware

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// server middleware
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// server
 const app = express();
 const port = process.env.PORT || 1337;
 
+// serve client files
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
 
+
+app.post('/api/oxfordComma', function(req, res) {
+  console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log(`I am listening to port #${port}`)
