2741dc88248f9db12d5035638ddea14cc7aabd67
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-12 13:51:18 -0700

    add interface to random fact child component

--- src/app/aboutme/randomfact/randomfact.component.ts
+++ src/app/aboutme/randomfact/randomfact.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { fact } from '../../interfaces/interfaces';
 @Component({
   selector: 'app-randomfact',
   templateUrl: './randomfact.component.html',
   styleUrls: ['./randomfact.component.css']
 })
 export class RandomfactComponent implements OnInit {
-  @Input('fact') fact: { link: string, info: string };
+  @Input('fact') fact: fact;
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
