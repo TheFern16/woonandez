16ba720160352528a314ea937048108f53509a8d
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-13 11:16:48 -0700

    refactor routes

--- helperFunctions/routes.js
+++ helperFunctions/routes.js
@@ -2,22 +2,24 @@ const express = require('express');
 const router = express.Router();
 const routes = router.route('/');
 
-app.get('/api/apps', (req, res) => {
+routes.get('/api/apps', (req, res) => {
   res.send(appData.apps);
 });
 
-app.get('/api/facts', (req, res) => {
+routes.get('/api/facts', (req, res) => {
   res.send(factData.facts);
 });
 
-app.get('/api/fun', (req, res) => {
+routes.get('/api/fun', (req, res) => {
   res.send(funData.fun);
 });
 
-app.get('/api/skills', (req, res) => {
+routes.get('/api/skills', (req, res) => {
   res.send(skillsData.skills);
 });
 
-app.get('*', (req, res) => {
+routes.get('*', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
 });
+
+module.exports = routes;
