bf50bd44a76264c0f9ad4fda7ff2ff8ae2b9b38a
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-09 12:04:21 -0700

    organize service methods

--- services/textHelpersService.js
+++ services/textHelpersService.js
@@ -1,43 +1,21 @@
 module.exports = {
-  oxfordComma: function(text) {
-    return new Promise((resolve, reject) => {
-      if (text.replace(/( and)/gi, ", and")) {
-        let testDouble = text.replace(/(,+)/gi, '');
-        let addOComma = testDouble.replace(/( and)/gi, ', and');
-        resolve(addOComma);
-      } else {
-        reject(Error('incorrect input'));
-      }
-    });
-  },
-  upperCase: function(text) {
+  altCase: function(text) {
     return new Promise((resolve, reject) => {
       if (text) {
-        resolve(text.toUpperCase());
+        const string = text.toLowerCase();
+        let result = '';
+        for (var i = 0; i < string.length; i++) {
+          if (i % 2 !== 0) {
+            result += string[i].toUpperCase();
+          } else {
+            result += string[i];
+          }
+        }
+        resolve(result);
       } else {
         reject(Error('incorrect input'));
       }
     });
-  },
-  lowerCase: function(text) {
-    return new Promise((resolve, reject) => {
-      if (text) {
-        resolve(text.toLowerCase());
-      } else {
-        reject(Error('incorrect input'));
-      }
-    });
-  },
-  createString: function(text) {
-    let strip = text.replace(/[^a-z ]/gi, '')
-    let first = strip.split(' ')[0].toLowerCase();
-    let second = strip
-      .toLowerCase()
-      .split(' ')
-      .slice(1)
-      .map(val => val[0].toUpperCase() + val.slice(1))
-      .join('');
-    return first.concat(second);
   },
   camelCase: function(text) {
     return new Promise((resolve, reject) => {
@@ -57,32 +35,43 @@ module.exports = {
     });
   },
   createString: function(text) {
-
+    let strip = text.replace(/[^a-z ]/gi, '')
+    let first = strip.split(' ')[0].toLowerCase();
+    let second = strip
+      .toLowerCase()
+      .split(' ')
+      .slice(1)
+      .map(val => val[0].toUpperCase() + val.slice(1))
+      .join('');
+    return first.concat(second);
   },
-  altCase: function(text) {
+  lowerCase: function(text) {
     return new Promise((resolve, reject) => {
       if (text) {
-        const string = text.toLowerCase();
-        let result = '';
-        for (var i = 0; i < string.length; i++) {
-          if (i % 2 !== 0) {
-            result += string[i].toUpperCase();
-          } else {
-            result += string[i];
-          }
-        }
-        resolve(result);
+        resolve(text.toLowerCase());
+      } else {
+        reject(Error('incorrect input'));
+      }
+    });
+  },
+  oxfordComma: function(text) {
+    return new Promise((resolve, reject) => {
+      if (text.replace(/( and)/gi, ", and")) {
+        let testDouble = text.replace(/(,+)/gi, '');
+        let addOComma = testDouble.replace(/( and)/gi, ', and');
+        resolve(addOComma);
+      } else {
+        reject(Error('incorrect input'));
+      }
+    });
+  },
+  upperCase: function(text) {
+    return new Promise((resolve, reject) => {
+      if (text) {
+        resolve(text.toUpperCase());
       } else {
         reject(Error('incorrect input'));
       }
     });
   }
 };
-
-// module.exports = {
-//   addOxfordComma,
-//   upperCase,
-//   lowerCase,
-//   camelCase,
-//   altCase
-// }
