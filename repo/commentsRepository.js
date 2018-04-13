c54816f5d7665925d2761fd5460c0f5e6407af09
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-13 14:46:28 -0700

    add find one and update methods

--- repo/commentsRepository.js
+++ repo/commentsRepository.js
@@ -1,6 +1,15 @@
 const woonandez = require('../db/connect.js');
 
 module.exports = {
+  findOne: function(id) {
+    woonandez: query({
+      name: 'find one',
+      text: 'SELECT * FROM COMMENTS WHERE comment_id = $1',
+      values: [id]
+    })
+    .then(result => result)
+    .catch(e => console.error(e));
+  },
   findAll: function() {
     return new Promise((res, rej) => {
       woonandez.query({
@@ -17,6 +26,17 @@ module.exports = {
         name: 'persist user',
         text: 'INSERT INTO COMMENTS(comment) VALUES ($1)',
         values: [body.comment]
+      })
+      .then(result => res(result))
+      .catch(e => console.error(e))
+    });
+  },
+  update: function(body) {
+    return new Promise((res, rej) => {
+      woonandez.query({
+        name: 'udate user',
+        text: 'INSERT INTO COMMENTS(comment) VALUES ($1) WHERE comment_id = $2',
+        values: [body.comment, body.comment_id]
       })
       .then(result => res(result))
       .catch(e => console.error(e))
