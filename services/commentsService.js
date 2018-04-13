19eca1e8b151a657443ccaa1f960473bcdcd8758
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-13 14:54:29 -0700

    add find one service method

--- services/commentsService.js
+++ services/commentsService.js
@@ -8,6 +8,13 @@ module.exports = {
         .catch(error => console.error(error));
     });
   },
+  findOne: function(req, res) {
+    return new Promise((resolve, reject) => {
+      commentsRepo.findOne(req.body.comment_id)
+        .then(response => resolve(response))
+        .catch(error => console.error(error))
+    })
+  },
   persist: function(req, res) {
     return new Promise((resolve, reject) => {
       commentsRepo.persist(req.body)
