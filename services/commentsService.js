a3cafb8e138b40d545a0c673f9c645c0f6b44315
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-23 18:41:44 -0700

    comment out comments secton until db is full implemented

--- services/commentsService.js
+++ services/commentsService.js
@@ -1,39 +1,39 @@
-const commentsRepo = require('../repo/commentsRepository.js');
+// const commentsRepo = require('../repo/commentsRepository.js');
 
-module.exports = {
-  delete: function(req, res) {
-    return new Promise((resolve, reject) => {
-      commentsRepo.delete(req.body)
-        .then(response => resolve(response))
-        .catch(error => console.error(error));
-    });
-  },
-  findAll: function() {
-    return new Promise((resolve, reject) => {
-      commentsRepo.findAll()
-        .then(response => resolve(response))
-        .catch(error => console.error(error));
-    });
-  },
-  findOne: function(req, res) {
-    return new Promise((resolve, reject) => {
-      commentsRepo.findOne(req.body.comment_id)
-        .then(response => resolve(response))
-        .catch(error => console.error(error))
-    })
-  },
-  persist: function(req, res) {
-    return new Promise((resolve, reject) => {
-      commentsRepo.persist(req.body)
-        .then(response => resolve(response))
-        .catch(error => console.error(error));
-    });
-  },
-  update: function(req, res) {
-    return new Promise((resolve, reject) => {
-      commentsRepo.update(req.body)
-        .then(response => resolve(response))
-        .catch(error => console.error(error))
-    });
-  }
-}
+// module.exports = {
+//   delete: function(req, res) {
+//     return new Promise((resolve, reject) => {
+//       commentsRepo.delete(req.body)
+//         .then(response => resolve(response))
+//         .catch(error => console.error(error));
+//     });
+//   },
+//   findAll: function() {
+//     return new Promise((resolve, reject) => {
+//       commentsRepo.findAll()
+//         .then(response => resolve(response))
+//         .catch(error => console.error(error));
+//     });
+//   },
+//   findOne: function(req, res) {
+//     return new Promise((resolve, reject) => {
+//       commentsRepo.findOne(req.body.comment_id)
+//         .then(response => resolve(response))
+//         .catch(error => console.error(error))
+//     })
+//   },
+//   persist: function(req, res) {
+//     return new Promise((resolve, reject) => {
+//       commentsRepo.persist(req.body)
+//         .then(response => resolve(response))
+//         .catch(error => console.error(error));
+//     });
+//   },
+//   update: function(req, res) {
+//     return new Promise((resolve, reject) => {
+//       commentsRepo.update(req.body)
+//         .then(response => resolve(response))
+//         .catch(error => console.error(error))
+//     });
+//   }
+// }
