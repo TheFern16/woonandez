d6fa0049ac0e585e93c407b471d3af1c77a5f6b5
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-23 18:32:50 -0700

    code cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { AboutmeComponent } from './components/aboutme/aboutme.component';
-// import { FunWithTextComponent } from './components/fun-with-text/fun-with-text.component';
-// import { GuestbookComponent } from './components/guestbook/guestbook.component';
-// import { HomeComponent } from './components/home/home.component';
-// import { ProjectsComponent } from './components/projects/projects.component';
-
-// const appRoutes: Routes = [
-//   { path: 'apps', component: ProjectsComponent },
-//   { path: 'aboutme', component: AboutmeComponent },
-//   { path: 'home', component: HomeComponent },
-//   { path: 'funWithText', component: FunWithTextComponent },
-//   { path: 'guestBook', component: GuestbookComponent },
-//   { path: '', redirectTo: '/home', pathMatch: 'full' },
-//   { path: '**', component: HomeComponent }
-// ]
-
 const lazyRoutes: Routes = [
   { path: 'aboutMe', loadChildren: './components/aboutme/aboutme.module#AboutMeModule' },
   { path: 'apps', loadChildren: './components/projects/projects.module#ProjectsModule' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,54 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Router, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-// import { HomeComponent } from './components/home/home.component';
-
-// import { AboutmeComponent } from './components/aboutme/aboutme.component';
-// import { ModalComponent } from './components/aboutme/modal/modal.component';
-// import { RandomfactComponent } from './components/aboutme/randomfact/randomfact.component';
-// import { TechComponent } from './components/aboutme/tech/tech.component';
-// import { FunWithTextComponent } from './components/fun-with-text/fun-with-text.component';
-// import { GuestbookComponent } from './components/guestbook/guestbook.component';
-
-// import { ProjectsComponent } from './components/projects/projects.component';
-// import { PhotoComponent } from './components/projects/photo/photo.component';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { RequestService } from './shared';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent
-    // HomeComponent,
-    // AboutmeComponent,
-    // ProjectsComponent,
-    // PhotoComponent,
-    // TechComponent,
-    // RandomfactComponent,
-    // GuestbookComponent,
-    // FunWithTextComponent,
-    // ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    HttpClientModule
   ],
   exports: [],
   providers: [ RequestService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {
-  constructor() {}
-}
+export class AppModule {}
