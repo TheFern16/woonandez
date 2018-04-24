94a11ac2ff9bf9d46bf410c928e6f1bc2e5f16d2
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-23 18:09:53 -0700

    fun with text should lazy load

--- src/app/components/fun-with-text/fun-with-text.module.ts
+++ src/app/components/fun-with-text/fun-with-text.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { FunWithTextComponent } from './fun-with-text.component';
 import { funWithTextRouting } from './fun-with-text.routing';
 
 @NgModule({
   imports: [
     CommonModule,
-    funWithTextRouting
+    funWithTextRouting,
+    FormsModule
   ],
   declarations: [ FunWithTextComponent ]
 })
