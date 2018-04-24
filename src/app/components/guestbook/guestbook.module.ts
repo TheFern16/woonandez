3aa909ef99cc2dccc77411b2eae67c4827e08846
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-23 18:11:26 -0700

    guestbook should lazy load

--- src/app/components/guestbook/guestbook.module.ts
+++ src/app/components/guestbook/guestbook.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { GuestbookComponent } from './guestbook.component';
 import { guestbookRouting } from './guestbook.routing';
 
 @NgModule({
   imports: [
     CommonModule,
-    guestbookRouting
+    guestbookRouting,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [ GuestbookComponent ]
 })
