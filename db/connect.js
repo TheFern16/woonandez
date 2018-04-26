06ac7667f2da4893b85a51bbffa205bf901408cf
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-26 16:51:54 -0700

    update env var

--- db/connect.js
+++ db/connect.js
@@ -6,7 +6,7 @@ const { Client } = require('pg');
 // });
 
 const client = new Client({
-  connectionString: process.event.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
