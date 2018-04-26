9c929180354d058cc7755412c15032ad18c944b1
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-26 16:35:49 -0700

    update con string

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,13 @@
 const { Client } = require('pg');
 // const conString = 'postgres://MatthewAFernandez:password@localhost:5432/woonandez';
 
+// const client = new Client({
+//   connectionString: conString || process.event.DATABASE_URL
+// });
+
 const client = new Client({
-  connectionString: conString || process.event.DATABASE_URL
+  connectionString: process.event.DATABASE_URL,
+  ssl: true
 });
 
 client.connect();
