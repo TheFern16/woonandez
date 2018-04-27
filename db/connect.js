bb26895cbbc2c3cf39d286cba727ecf26480b945
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-26 17:33:51 -0700

    update and error handle

--- db/connect.js
+++ db/connect.js
@@ -4,11 +4,12 @@ const client = new Client({});
 
 if (process.env.DATABASE_URL === undefined) {
   client.connectionString ='postgres://MatthewAFernandez:password@localhost:5432/woonandez';
+  client.connect();
+  module.exports = client;
 } else {
   client.connectionString =  process.env.DATABASE_URL;
   client.ssl = true;
+  client.connect();
+  module.exports = client;
 };
 
-client.connect();
-
-module.exports = client;
