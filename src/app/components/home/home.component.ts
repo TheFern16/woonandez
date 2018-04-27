0f03d582ab217a68525183e8eba5dd7dbc6039b0
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-26 17:10:30 -0700

    remove modal from home comp

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,25 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  showModal: boolean = false;
-
   constructor() { }
 
   ngOnInit() {}
-
-  getStyle() {
-    if (this.showModal === false) {
-      return 'none';
-    } else {
-      return 'block';
-    }
-  }
-
-  toggleModal(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('modalWrapper') || e.target.classList.contains('btn')) {
-      this.showModal = !this.showModal;
-    }
-  }
-
 }
