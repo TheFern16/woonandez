de50d2047401ab2d208cb43be15a0e55fe0a5c03
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-30 15:58:19 -0700

    update fwt comp and hide con string

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const conString = 'postgres://MatthewAFernandez:password@localhost:5432/woonandez';
+const conString = require('./con.js');
 const client = new Client({
   connectionString: process.env.DATABASE_URL === undefined ? conString : process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL === undefined ? false : true
