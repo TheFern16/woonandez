e2b737d97e99ac187c21c05bfdff60eb056e8ce3
woonandez <matthew.allen.fernandez@gmail.com>
2018-05-02 15:39:40 -0700

    add route for applications module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const lazyRoutes: Routes = [
   { path: 'aboutMe', loadChildren: './components/aboutme/aboutme.module#AboutMeModule' },
-  { path: 'apps', loadChildren: './components/projects/projects.module#ProjectsModule' },
+  // { path: 'apps', loadChildren: './components/projects/projects.module#ProjectsModule' },
+  { path: 'apps', loadChildren: './components/applications/applications.module#ApplicationsModule' },
   { path: 'funWithText', loadChildren: './components/funWithText/funWithText.module#FunWithTextModule' },
   { path: 'guestBook', loadChildren: './components/guestbook/guestbook.module#GuestbookModule'},
   { path: 'home', loadChildren: './components/home/home.module#HomeModule' },
